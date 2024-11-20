9b950d419200a88f1bddecbbe383de725dcdc26a
Sávio Félix Marques <savio.marques@sap.com>
2024-11-20 14:26:24 -0300

    first commit

---
+++ app/capaichatagro-ui/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("capaichatagroui.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
